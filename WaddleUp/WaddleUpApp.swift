74f31fb1e10f17131f9c8a1f68488b0af7ca72de
ivanrdnv <ivzavertkin@gmail.com>
2024-01-25 11:21:34 +0300

    Initial commit. Add .gitignore file

---
+++ WaddleUp/WaddleUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaddleUpApp.swift
+//  WaddleUp
+//
+//  Created by ivan on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WaddleUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
